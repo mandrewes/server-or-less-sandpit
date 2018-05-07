da1fd7775065eb5b5b4d6889db59d633a37a05d1
Michael Andrewes <mandrewes@roguesquadron.com.au>
2018-05-07 14:46:01 +1000

    none

--- src/main/java/com/yolo/example/serverorless/Application.java
+++ src/main/java/com/yolo/example/serverorless/Application.java
@@ -1,6 +1,8 @@
 package com.yolo.example.serverorless;
 
 
+import org.springframework.context.annotation.ImportResource;
+
 /** Simple class to start up the application.
  *
  * @SpringBootApplication adds:
@@ -9,9 +11,9 @@ package com.yolo.example.serverorless;
  *  @ComponentScan
  */
 // @SpringBootApplication(scanBasePackages = {"com.aws.codestar", "tech.rsqn.cdsl"})
-// @ImportResource("classpath:/spring/app-ctx.xml")
+@ImportResource("classpath:/spring/app-ctx.xml")
 public class Application {
     public static void main(String[] args) {
-        // SpringApplication.run(Application.class, args);
+//         SpringApplication.run(Application.class, args);
     }
 }
