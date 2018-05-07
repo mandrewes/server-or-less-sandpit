3e3d8d6df5b0afb60b881b74997d18f9e39cfac2
Michael Andrewes <mandrewes@roguesquadron.com.au>
2018-05-07 17:17:25 +1000

    none

--- src/main/java/com/yolo/example/serverorless/Application.java
+++ src/main/java/com/yolo/example/serverorless/Application.java
@@ -2,19 +2,21 @@ package com.yolo.example.serverorless;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
-/** Simple class to start up the application.
+/**
+ * Simple class to start up the application.
  *
  * @SpringBootApplication adds:
- *  @Configuration
- *  @EnableAutoConfiguration
- *  @ComponentScan
+ * @Configuration
+ * @EnableAutoConfiguration
+ * @ComponentScan
  */
-// @SpringBootApplication(scanBasePackages = {"com.aws.codestar", "tech.rsqn.cdsl"})
+@SpringBootApplication(scanBasePackages = {"tech.rsqn.cdsl"})
 @ImportResource("classpath:/spring/test-app-ctx.xml")
 public class Application {
     public static void main(String[] args) {
-         SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
