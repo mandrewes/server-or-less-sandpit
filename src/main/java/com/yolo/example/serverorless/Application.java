18b3c21650a2f6a619aff6ce08511c7553925cbf
Michael Andrewes <mandrewes@roguesquadron.com.au>
2018-05-08 11:28:00 +1000

    none

--- src/main/java/com/yolo/example/serverorless/Application.java
+++ src/main/java/com/yolo/example/serverorless/Application.java
@@ -13,8 +13,8 @@ import org.springframework.context.annotation.ImportResource;
  * @EnableAutoConfiguration
  * @ComponentScan
  */
-@SpringBootApplication(scanBasePackages = {"tech.rsqn.cdsl"})
-@ImportResource("classpath:/spring/test-app-ctx.xml")
+@SpringBootApplication(scanBasePackages = {"com.yolo.example.serverorless.springboot"})
+@ImportResource("classpath:/spring/app-ctx.xml")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
