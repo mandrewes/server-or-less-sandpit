ba6191be17afeafe647db928be20731b813deb9f
Michael Andrewes <mandrewes@roguesquadron.com.au>
2018-05-08 13:30:35 +1000

    none

--- src/main/java/com/yolo/example/serverorless/springboot/SaveStuffRestFacade.java
+++ src/main/java/com/yolo/example/serverorless/springboot/SaveStuffRestFacade.java
@@ -1,18 +1,38 @@
 package com.yolo.example.serverorless.springboot;
 
+import com.yolo.example.serverorless.lambda.ListStuffLambdaFacade;
+import com.yolo.example.serverorless.model.Stuff;
+import com.yolo.example.serverorless.services.logic.StuffService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 @EnableAutoConfiguration
 public class SaveStuffRestFacade {
 
+    private static final Logger LOG = Logger.getLogger(ListStuffLambdaFacade.class);
+
+    @Autowired
+    private StuffService stuffService;
+
     @RequestMapping("/")
     @ResponseBody
-    String home() {
+    public String home() {
         return "Hello World!";
+    }
+
+
+    @RequestMapping("/list")
+    @ResponseBody
+    public List<Stuff> listStuff() {
+        List<Stuff> ret = stuffService.listStuff();
+        return ret;
     }
 
 //    public static void main(String[] args) throws Exception {
